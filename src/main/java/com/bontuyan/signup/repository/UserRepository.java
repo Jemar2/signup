0af8fbfd8565044ddaaa0e59de8c6c180392ff8b
JEMAR_Sysarch <imusjemar@gmail.com>
2024-07-04 22:04:58 +0800

    Initial commit

---
+++ src/main/java/com/bontuyan/signup/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bontuyan.signup.repository;
+
+import com.bontuyan.signup.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsername(String username);
+}
---
+++ src/test/java/com/bontuyan/signup/SignupSysarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bontuyan.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupSysarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
